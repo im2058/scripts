200cccb0b2d4a1ff3b7af1ae12bdbaf94a189604
im2058 <10438075@qq.com>
2018-09-21 22:26:43 +0800

    ./*

--- scripts/first_install/first.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#sudo cp ./source.list /etc/apt/
-#sudo apt-get update
-#sudo apt-get upgrade
-sudo apt-get install -y vim tmux
-
-# samba
-sudo apt-get install -y samba shadowsocks privoxy python-pip
--- scripts/first_install/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# for auto mount usb>>/mnt/usb for samba
-usb=`ls /dev/sd*4`
-sudo mount -t vfat -o umask=000 $usb /mnt/usb
-
-# for youtube download
-source /home/pi/KK/youtube/privoxy_rc
-sslocal -c /etc/shadowsocks/config.json &  >> sslocal.log
